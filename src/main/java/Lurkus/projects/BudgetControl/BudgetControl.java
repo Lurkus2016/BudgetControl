67e059bce61ff2811951c7634208e562f0f5ec8e
Lurkus2016 <leoniddudin89@mail.ru>
2017-07-29 15:40:18 +0300

    Initial commit

---
+++ src/main/java/Lurkus/projects/BudgetControl/BudgetControl.java
@@ -0,0 +1,11 @@
+package Lurkus.projects.BudgetControl;
+
+/**
+ * Created by VR2 on 29.07.2017.
+ */
+public class BudgetControl {
+
+    public static void main(String args[]){
+        System.out.println("Starting...");
+    }
+}
